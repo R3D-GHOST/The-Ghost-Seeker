e2e66da3c8fe64ca6d5f939e4d4d7e3dd1e367d6
redghost <94316140+R3D-GHOST@users.noreply.github.com>
2023-02-04 20:53:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ echo "Instalando Repositorios"
 sudo apt install python3 
 sudo apt install pip3
 pip3 install maigret
-git clone https://github.com/sundowndev/PhoneInfoga
-cd PhoneInfoga/
-python3 -m pip install -r requirements.txt
-
+mkdir phone
+cd phone
+curl -sSL https://raw.githubusercontent.com/sundowndev/phoneinfoga/master/support/scripts/install | bash 
 sudo rm -rf install.sh
